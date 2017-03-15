ddd2e97f24264444bad168199849acf1d23c913a
Alvince Pongos <alvincepongos@gmail.com>
2017-03-15 11:29:22 -0400

    Change views
    Changed views from non-generic types to be able to handle specific
    method calls to get certain objects

--- Univ/ListIndex/urls.py
+++ Univ/ListIndex/urls.py
@@ -4,8 +4,9 @@ from . import views
 
 app_name = 'ListIndex'
 urlpatterns = [
-    url(r'^$', views.IndexView.as_view(), name='index'),
+    #url(r'^$', views.IndexView.as_view(), name='index'),
     #url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    url(r'^$', views.index, name='index'),
     url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
 
 ]
--- Univ/ListIndex/views.py
+++ Univ/ListIndex/views.py
@@ -5,7 +5,7 @@ from django.views import generic
 from django.forms import ModelForm
 
 
-from .models import UniversitydataCollegedata
+from .models import UniversitydataCollegedata, Statedemographics
 
 class IndexView(generic.ListView):
     template_name = 'ListIndex/index.html'
@@ -15,9 +15,33 @@ class IndexView(generic.ListView):
 	    """Return the last five published questions."""
 	    return [UniversitydataCollegedata.objects.filter(female__lte=98).order_by('-female')[:10], UniversitydataCollegedata.objects.filter(male__lte = 98).order_by('-male')[:10], UniversitydataCollegedata.objects.order_by('-american_indian_alaskan_native')[:10]]
 
+
+def index(request):
+	queryset_list = UniversitydataCollegedata.objects.all()
+	q2 = Statedemographics.object.all()
+	stateInfo = {}
+
+	latest_question_list= [UniversitydataCollegedata.objects.filter(female__lte=98).order_by('-female')[:10], UniversitydataCollegedata.objects.filter(male__lte = 98).order_by('-male')[:10], UniversitydataCollegedata.objects.order_by('-american_indian_alaskan_native')[:10]]
+	
+	for objects in latest_question_list: #Match state data
+		stateInfo[objects.university] = q2.objects.filter(objects.state)
+
+	for objects in latest_question_list[0]:
+		objects = 
+	latest_question_list.append
+	context = {'latest_question_list':latest_question_list}
+
+	query =request.GET.get("q")
+	if query:
+		queryset_list = queryset_list.filter(university__icontains=query)
+
+	return render(request, 'ListIndex/index.html', context, queryset_list)
+
 def detail(request, question_id):
     question = UniversitydataCollegedata.objects.filter(id=question_id).values()[0]
     return render(request, 'ListIndex/detail.html', {'question': question})
+
+
 # class DetailView(generic.DetailView):
 # 	model = UniversitydataCollegedata
 # 	template_name = 'ListIndex/detail.html'
