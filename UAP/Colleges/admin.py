092de1ab033bace9ec35aeb0fb33bba124e35ca7
andrew-lockwood <andrewlockwood579@gmail.com>
2017-04-14 00:38:25 -0400

    use timezone

--- UAP/Colleges/admin.py
+++ UAP/Colleges/admin.py
@@ -1,1 +1,11 @@
 from django.contrib import admin
+from .models import UniversitydataCollegedata, Statedemographics
+
+
+@admin.register(UniversitydataCollegedata)
+class CollegeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'university', 'state_location')
+    empty_value_display = '-empty-'
+
+    def state_location(self, instance):
+        return instance.state.location
