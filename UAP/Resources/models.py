05c9492dd1e838824ebba616db7724dfb6e4fdde
Alvince Pongos <alvincepongos@gmail.com>
2017-04-16 14:41:15 -0400

    Read_Time bug for admin

--- UAP/Resources/models.py
+++ UAP/Resources/models.py
@@ -101,8 +101,8 @@ def pre_save_post_receiver(sender, instance, *args, **kwargs):
 
     if instance.content:
         html_string = instance.get_markdown()
-        read_time_var = get_read_time(html_string)
-        instance.read_time = read_time_var
+        #read_time_var = get_read_time(html_string)
+        #instance.read_time = read_time_var
 
 
 pre_save.connect(pre_save_post_receiver, sender=Post)
